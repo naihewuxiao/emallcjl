78f4cb30c399e8002a872d6f60d477abe0ad75ff
cjl10123 <181199610@qq.com>
2020-05-02 11:40:09 +0800

    update

---
+++ emall-item-web/src/main/java/com/cjl/emall/item/EmallItemWebApplication.java
@@ -0,0 +1,13 @@
+package com.cjl.emall.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallItemWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallItemWebApplication.class, args);
+    }
+
+}
---
+++ emall-item-web/src/test/java/com/cjl/emall/item/EmallItemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjl.emall.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallItemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
