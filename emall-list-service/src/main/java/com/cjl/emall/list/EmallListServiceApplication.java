78dcbefc0f14ff9bcdcb3ed5a8f4c047d3f7a012
cjl10123 <181199610@qq.com>
2020-05-06 11:27:18 +0800

    update

---
+++ emall-list-service/src/main/java/com/cjl/emall/list/EmallListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cjl.emall.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallListServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallListServiceApplication.class, args);
+    }
+
+}
